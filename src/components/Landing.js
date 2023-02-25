14bd0e0b7574edc82abb2a0bc7be09fff82aabb7
Zein19 <zein.23@hotmail.co.uk>
2023-02-25 12:23:09 +0000

    Made navbar and landing components

---
+++ src/components/Landing.js
@@ -0,0 +1,9 @@
+function Landing(){
+    return(
+        <section className="landing">
+            <h1 className="landing-title">Hi, my name is <span>Zein</span> and I am a software developer.</h1>
+        </section>
+    )
+}
+
+export default Landing
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+function Navbar(){
+    return(
+        <header className="navbar">
+            <nav>
+                <a href="github.com">ZS</a>
+                <ul>
+                    <li className="nav-item"><a href="github.com">About</a></li>
+                    <li className="nav-item"><a href="github.com">Projects</a></li>
+                    <li className="nav-item"><a href="github.com">Contact Me</a></li>
+                </ul>
+                <a href="github.com">My CV</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
