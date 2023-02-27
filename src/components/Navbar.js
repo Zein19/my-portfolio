b3418064d6ba0a634d0dc9a1c8278ba0b07790cd
Zein19 <zein.23@hotmail.co.uk>
2023-02-27 09:52:22 +0000

    made adjustments to make site responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,29 @@
+import React from "react";
+
 function Navbar(){
+
+    const [showNav, setShowNav] = React.useState(false);
+
+    function toggleNavBar(){
+      setShowNav(!showNav);
+    }
+
     return(
         <header className="navbar">
             <nav>
-                <a href="#top">ZS</a>
-                <ul>
+                
+                <ul className={showNav ? "show" : ""}>
+                    <li className="nav-item"><a href="#top">ZS</a></li>
                     <li className="nav-item"><a href="#about">About</a></li>
                     <li className="nav-item"><a href="#projects">Projects</a></li>
                     <li className="nav-item"><a href="#contact">Contact Me</a></li>
+                    <li className="nav-item"><a href="CV" download="ZeinSharif-CV">My CV</a></li>
                 </ul>
-                <a href="CV" download="ZeinSharif-CV">My CV</a>
+                
+                { showNav ? null :(
+                    <button className="nav-toggle" onClick={toggleNavBar}>
+                    <img src="/images/navtoggle.png" alt="nav toggle" />
+                </button>)}
             </nav>
         </header>
     )
