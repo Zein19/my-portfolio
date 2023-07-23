7c20752643019f2e960687eb0d0c6571aa4d705e
Zein19 <zein.23@hotmail.co.uk>
2023-07-23 10:04:30 +0100

    updated portfolio and added twitter contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,7 @@ function Contact(){
             <ul className="contact-info">
                 <li className="contact-item">Email: <a href="mailto:zein.23@hotmail.co.uk" className="contact-link">zein.23@hotmail.co.uk</a></li>
                 <li className="contact-item">LinkedIn: <a href="https://www.linkedin.com/in/zein-sharif-15840a230/" rel="noreferrer" target="_blank" className="contact-link">Zein Sharif</a></li>
+                <li className="contact-item">Twitter: <a href="https://twitter.com/zeinsharif_" target="_blank" rel="noreferrer" className="contact-link">@zeinsharif_</a></li>
                 <li className="contact-item">GitHub: <a href="https://github.com/Zein19" target="_blank" rel="noreferrer" className="contact-link">Zein19</a></li>
             </ul>
         </section>
