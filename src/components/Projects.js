488b261c4146d9ff8ed0f579b05260f862336dec
Zein19 <zein.23@hotmail.co.uk>
2023-08-06 22:06:01 +0100

    updated cv

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,27 +2,27 @@ function Projects(){
     const projectList = [
         {
           key: 1,
-          name: 'Meme Generator',
-          description: 'A meme generator web app made using React. The memes were fetched from a meme api and the page was styled using CSS. The app generates a random meme from the API, and has two inputs where the top and bottom text on the meme can be added.',
-          image: '/images/meme-generator.png',
-          link: 'https://meme-generator-zs.netlify.app/',
-          source:'https://github.com/Zein19/meme-generator'
+          name: 'NBA Player Stats app',
+          description: 'An nba player stats web app made using React. Using the balldontlie api, I fetched the player data. Also made use of hooks. I still intend to add to this project, and one improvement I am planning is to be able to select stats from different seasons, as it is currently only using the 2022 season.',
+          image: '/images/nba-app.png',
+          link: 'https://zbs-nbasearchapp.netlify.app/',
+          source:'https://github.com/Zein19/NBA-Search-app'
         },
         {
           key: 2,
-          name: 'Travel Journal',
-          description: 'A travel journal made using react. The destinations are contained in a seperate data file and mapped onto the app, so they can be updated and the page would update accordingly, instead of having to hardcode the details.',
-          image: '/images/travel-journal.png',
-          link: 'https://travel-journal-zs.netlify.app/',
-          source:'https://github.com/Zein19/travel-journal'
+          name: 'Weather App',
+          description: 'A weather app made using React which allows you to search for the weather in real time of any city around the world. Made use of hooks, and also terniary to change the background depending on the temperature.',
+          image: '/images/weather-app.png',
+          link: 'https://zbs-weatherapp.netlify.app/',
+          source:'https://github.com/Zein19/Weather-app'
         },
         {
           key: 3,
-          name: 'Tenzies',
-          description: 'A tenzies game web app. The game is essentially won when you manage to roll 10 of the same number as soon as possible. I used React and CSS to style, and made use of the useEffect and useState hooks to enable the game to function.',
-          image: '/images/tenzies.png',
-          link: 'https://tenzies-zs.netlify.app/',
-          source:'https://github.com/Zein19/Tenzies'
+          name: 'Movie database app',
+          description: 'A movie data base app made using HTML, CSS and vanilla JavaScript. It includes the poster, rating and summary of the films. Made use of the moviedb api. Possible improvement I plan to make in future is to be able to sort the movies by rating.',
+          image: '/images/movie-app.png',
+          link: 'https://zbs-moviesearch.netlify.app/',
+          source:'https://github.com/Zein19/MovieSearchApp'
         }
       ];
 
